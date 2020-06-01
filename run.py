dbc51c336fcc06429c844d8aa9b7e7930af4d5f0
DmitriyBessalov <42869870+DmitriyBessalov@users.noreply.github.com>
2020-06-01 17:28:38 +0300

    Update run.py
    -

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 ''' Скрипт для уведомлений о новых вакансиях '''
 import requests
 from bs4 import BeautifulSoup as BS
@@ -6,42 +7,56 @@ import sqlite3
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import telepot
+import os
 
 def get_requests(url, base_url, selector, metod=''):
     if metod == 'post':
         answ = s.post(url, data=postdata, headers=headers)
     else:
-        answ = s.get(url, headers=headers)
+        try:
+            answ = s.get(url, headers=headers)
+        except:
+            print('not get data: ' + url)
+            return
+
     ans_bs = BS(answ.content, 'html.parser')
     new = ans_bs.select(selector)
     for elem in new:
         new_link[base_url + elem.attrs['href']] = elem.text
 
+
 def send_mail(links={}):
-    addr_from = "bez1dn6a@mail.ru"                      # Адресат
-    addr_to = "bez1dn6a@yandex.ru"                      # Получатель
-    password = "\\\ пароль скрыт ///"                   # Пароль
-    msg = MIMEMultipart()                               # Создаем сообщение
-    msg['From'] = addr_from                             # Адресат
-    msg['To'] = addr_to                                 # Получатель
-    msg['Subject'] = 'Ссылки по работе'                 # Тема сообщения
-    html = """
-    <html>
-      <head></head>
-      <body>"""
+     addr_from = "bez1dn6a@mail.ru"                      # Адресат
+     addr_to = "bez1dn6a@yandex.ru"                      # Получатель
+     password = "************"                           # Пароль
+     msg = MIMEMultipart()                               # Создаем сообщение
+     msg['From'] = addr_from                             # Адресат
+     msg['To'] = addr_to                                 # Получатель
+     msg['Subject'] = 'Ссылки по работе'                 # Тема сообщения
+     html = """
+     <html>
+       <head></head>
+       <body>"""
+     for elem in links:
+         html = html + '<a href="' + elem + '">' + links[elem] + '</a><br>'
+     html = html + """
+       </body>
+     </html>
+     """
+     msg.attach(MIMEText(html, 'html', 'utf-8'))         # Добавляем в сообщение HTML-фрагмент
+     server = smtplib.SMTP('smtp.mail.ru', 25)           # Создаем объект SMTP
+     server.set_debuglevel(False)                        # Выключаем режим отладки
+     server.starttls()                                   # Начинаем шифрованный обмен по TLS
+     server.login(addr_from, password)                   # Получаем доступ
+     server.send_message(msg)                            # Отправляем сообщение
+     server.quit()                                       # Выходим
+
+def send_telegram(links={}):
+    TelegramBot = telepot.Bot('*****************')
     for elem in links:
-        html = html + '<a href="' + elem + '">' + links[elem] + '</a><br>'
-    html = html + """
-      </body>
-    </html>
-    """
-    msg.attach(MIMEText(html, 'html', 'utf-8'))         # Добавляем в сообщение HTML-фрагмент
-    server = smtplib.SMTP('smtp.mail.ru', 25)           # Создаем объект SMTP
-    server.set_debuglevel(False)                        # Выключаем режим отладки
-    server.starttls()                                   # Начинаем шифрованный обмен по TLS
-    server.login(addr_from, password)                   # Получаем доступ
-    server.send_message(msg)                            # Отправляем сообщение
-    server.quit()                                       # Выходим
+        TelegramBot.sendMessage(297036937, elem)
+    #    html = html + '<a href="' + elem + '">' + links[elem] + '</a><br>'
 
 
 new_link = {}
@@ -81,42 +96,61 @@ postdata = {
     'u_token_key': 'a8dcb7e72c80fef5394da9732d04e9d9'
 }
 
-
 i = -1
 while 1:
     i += 1
-    if i % 4 == 0:
-        print('all')
-        get_requests('https://krasnodar.hh.ru/search/vacancy?search_period=7&clusters=true&area=1438&text=Python&order_by=publication_time&enable_snippets=true', '', 'a.bloko-link.HH-LinkModifier')
-        get_requests('https://krasnodar.hh.ru/search/vacancy?clusters=true&enable_snippets=true&order_by=publication_time&schedule=remote&search_period=3&text=Python&L_save_area=true&area=113&from=cluster_area&showClusters=true', '', 'a.bloko-link.HH-LinkModifier')
-        get_requests('https://career.habr.com/vacancies?city_id=707&type=all', 'https://career.habr.com/', 'div.vacancy-card__title>a')
-        get_requests('https://career.habr.com/vacancies?q=python&remote=true&type=all', 'https://career.habr.com/', 'div.vacancy-card__title>a')
-        get_requests('https://russia.superjob.ru/vacancy/search/?keywords=python&remote_work=1', 'https://russia.superjob.ru', 'a._1UJAN')
-        get_requests('https://rabota.yandex.ru/krasnodarskiy_kray/vakansii/?text=python&top_days=7&sort=cr_date', '', 'a.link.serp-vacancy__name.stat__click')
-    print(i, 'fl')
+    print('fl', i)
     postdata['pf_keywords'] = 'python'
     get_requests('https://www.fl.ru/projects/', 'https://www.fl.ru', 'a.b-post__link', 'post')
     postdata['pf_keywords'] = 'django'
-    get_requests('https://www.fl.ru/projects/', 'https://www.fl.ru','a.b-post__link', 'post')
+    get_requests('https://www.fl.ru/projects/', 'https://www.fl.ru', 'a.b-post__link', 'post')
+
+    if i % 4 == 0:
+        print('all')
+        get_requests('https://krasnodar.hh.ru/search/vacancy?search_period=7&clusters=true&area=1438&text=Python&order_by=publication_time&enable_snippets=true', '', 'a.bloko-link.HH-LinkModifier')
+        # get_requests('https://krasnodar.hh.ru/search/vacancy?clusters=true&enable_snippets=true&order_by=publication_time&schedule=remote&search_period=3&text=Python&L_save_area=true&area=113&from=cluster_area&showClusters=true', '', 'a.bloko-link.HH-LinkModifier')
+        get_requests('https://career.habr.com/vacancies?city_id=707&type=all', 'https://career.habr.com/', 'div.vacancy-card__title>a')
+        # get_requests('https://career.habr.com/vacancies?q=python&remote=true&type=all', 'https://career.habr.com/', 'div.vacancy-card__title>a')
+        # get_requests('https://russia.superjob.ru/vacancy/search/?keywords=python&remote_work=1', 'https://russia.superjob.ru', 'a._1UJAN')
+        get_requests('https://rabota.yandex.ru/krasnodarskiy_kray/vakansii/?text=python&top_days=7&sort=cr_date', '', 'a.link.serp-vacancy__name.stat__click')
 
     for link in new_link:
 
         conn = sqlite3.connect('mydatabase.db')
 
         cursorObj = conn.cursor()
-        # cursorObj.execute("CREATE TABLE if not exists vacancy(url text PRIMARY KEY, title text )")
+        cursorObj.execute("CREATE TABLE if not exists vacancy(url text PRIMARY KEY, title text , create_datetime text)")
         cursorObj.execute("SELECT url FROM vacancy WHERE url = :link", {"link": link})
 
         row = cursorObj.fetchone()
         if None == row:
-            cursorObj.execute("INSERT INTO vacancy VALUES (?, ?)", (link, new_link[link]))
+            cursorObj.execute("INSERT INTO vacancy VALUES (?, ?, DateTime('now', 'localtime'))", (link, new_link[link]))
             conn.commit()
             print(link, new_link[link])
+
+            os.system('google-chrome-stable "' + link + '"')
+
+            try:
+                os.system('notify-send "' + new_link[link] + '"')
+            except:
+                print(link, new_link[link])
+
             send_link[link] = new_link[link]
+            time.sleep(8)
         conn.close()
 
-    send_mail(send_link)
+    # try:
+    #     send_mail(send_link)
+    # except:
+    #     print('email to spam banned')
+
+    # try:
+    #    send_telegram(send_link)
+    # except:
+    #     print('telegramm not connected')
+
     new_link.clear()
     send_link.clear()
 
+    print('wait 5 min')
     time.sleep(300)
